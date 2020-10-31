b102a135e39682d3e821f5d80ab61d4f3e4d74ec
Nicholas Ocket <nicholas.ocket@lightspeedhq.com>
2020-10-31 09:43:45 +0100

    first commit: fixing the gitignore file

---
+++ src/test/java/be/about/coding/codequality/CodeQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package be.about.coding.codequality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeQualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
